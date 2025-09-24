52e3910d925c145054118eada45cadbe2a0bd347
Walterb0b <osca0001@stud.ek.dk>
2025-09-24 09:27:43 +0200

    user

--- src/User.java
+++ src/User.java
@@ -2,5 +2,16 @@ public class User {
     private boolean isPremium;
     private String userName;
 
+    User(String userName, boolean isPremium){
+        this.userName = userName;
+        this.isPremium = isPremium;
+    }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
 }
